27c563f4bfb449847cbeab669dc2318aa14149a1
Kofi Twum-Ampofo <pktwum96@gmail.com>
2020-07-25 09:50:48 +0100

    cypress testing

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -4,6 +4,9 @@ describe("Calculator app Interface", () => {
   beforeEach(() => {
     cy.visit("/");
   });
+  it("should render", () => {
+    cy.get('[data-cy="mobile-screen"]').screenshot("App Day Mode");
+  });
   describe("night mode button", () => {
     it("renders", () => {
       cy.get('[data-cy="night-mode-button"]').should("be.visible");
@@ -33,10 +36,17 @@ describe("Calculator app Interface", () => {
         "color",
         "rgb(255, 255, 255)"
       );
+      cy.get('[data-cy="mobile-screen"]').screenshot("App Night Mode");
     });
 
     it("should change back to day mode on second click", () => {
-      cy.get('[data-cy="night-mode-button"]').dblclick();
+      cy.get('[data-cy="night-mode-button"]').click();
+      cy.get('[data-cy="mobile-screen"]').should(
+        "have.css",
+        "background-color",
+        "rgb(26, 26, 26)"
+      );
+      cy.get('[data-cy="night-mode-button"]').click();
       cy.get('[data-cy="mobile-screen"]').should(
         "have.css",
         "background-color",
@@ -107,12 +117,53 @@ describe("Calculator app Interface", () => {
     });
   });
 
-  describe("results interface", () => {
-    it("equal sign should not render until there is a result", () => {
-      cy.get('[data-cy="equal-sign-icon"]').should("not.exist");
+  describe("action buttons", () => {
+    beforeEach(() => {
+      cy.get('[data-cy="results"]').as("Results");
+      cy.get('[data-cy="display-user-input"]').as("UserInput");
+      cy.get('[data-cy="action-button-backspace"]').as("Backspace");
+      cy.get('[data-cy="action-button-backspace"]').as("Backspace");
+      cy.get('[data-cy="action-button-clear"]').as("Clear");
       cy.get('[data-cy="input-button-9"]').click();
-      cy.get("[data-cy='action-button-equals']").click();
-      cy.get('[data-cy="equal-sign-icon"]').should("be.visible");
+      cy.get('[data-cy="action-button-minus"]').click().as("Minus");
+      cy.get('[data-cy="input-button-0"]').click();
+      cy.get('[data-cy="input-button-."]').click();
+      cy.get('[data-cy="input-button-1"]').click();
+      cy.get('[data-cy="action-button-equals"]').click().as("Equals");
+      cy.get('[data-cy="input-button-9"]').click();
+      cy.get('[data-cy="action-button-times"]').click();
+      cy.get('[data-cy="input-button-2"]').click();
+    });
+    describe("equal buttons", () => {
+      it("should evaluate clicked buttons", () => {
+        cy.get("@Equals").click();
+        cy.get("@Results").should("have.text", "18");
+        cy.get("@Minus").click();
+        cy.get('[data-cy="input-button-2"]').click();
+        cy.get("@Equals").click();
+        cy.get("@Results").should("have.text", "16");
+      });
+    });
+    describe("backspace button", () => {
+      it("should clear last typed element", () => {
+        cy.get("@Backspace").click();
+        cy.get("@UserInput").should("have.text", "9x");
+        cy.get("@Backspace").click();
+        cy.get("@UserInput").should("have.text", "9");
+      });
+    });
+    describe("clear button", () => {
+      it("should clear last typed element", () => {
+        cy.get("@Clear").click();
+        cy.get("@UserInput").should("have.text", "");
+      });
     });
   });
+
+  // describe("", () => {
+  //   it("", () => {});
+  // });
+  // describe("", () => {
+  //   it("", () => {});
+  // });
 });
